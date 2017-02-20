71d6c5d29caeb3b1f9f4dfc95b26413f6fc133d8
Garrett Holmes <Garrett.Holmes@gmail.com>
2017-02-19 22:51:12 -0600

    utils module, style change

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,9 @@ import { Router } from 'preact-router';
 import Header from './header';
 import Home from './home';
 import Profile from './profile';
+import InfoBox from './InfoBox';
+import { getCookieByKey } from './lib/utils';
+
 
 export default class App extends Component {
 	/** Gets fired when the route changes.
@@ -14,14 +17,33 @@ export default class App extends Component {
 		this.currentUrl = e.url;
 	};
 
+	constructor(){
+		super();
+		
+	var uuid = window.localStorage.getItem('uuid');
+	if(uuid === null) {
+
+	}
+
+
+	if(uuid === null && cookie.includes('uuid=')) {
+		
+		window.localStorage.setItem('uuid',uuid);
+	} else if(!cookie.includes('uuid=') && uuid !== null) {
+			
+	}
+
+		this.state = {
+				uuid
+			};
+	}
+
 	render() {
 		return (
 			<div id="app">
 				<Header />
 				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
+					<InfoBox path="/" uuid={this.state.uuid} />
 				</Router>
 			</div>
 		);
---
+++ src/lib/utils.js
@@ -0,0 +1,13 @@
+var getCookieByKey = function(key){
+	var cookie = document.cookie;
+	if(!cookie.includes(key)){
+		return '';
+	} else {
+		var startIndex = cookie.indexOf(`${key}=`)+key.length+1;
+		var endIndex = cookie.slice(startIndex).indexOf('&') === -1 ? cookie.length : cookie.indexOf('&');
+		var val = cookie.slice(startIndex,endIndex);
+		return val;
+	}
+};
+
+export { getCookieByKey};
