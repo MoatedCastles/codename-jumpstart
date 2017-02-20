67643dd112e38c924113e59217d7559cefbb9394
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-02-20 13:28:21 -0600

    new transaction helper and endpoint fleshed out

--- server/helpers/transaction-helpers.js
+++ server/helpers/transaction-helpers.js
@@ -1,8 +1,44 @@
 import Transactions from '../model/transactions';
+import { getNextCard } from './inventory-helpers';
 
-var createTransaction = function(uuid, callback){
-  
+var createTransaction = function(btcAddress, uuid, quantity, callback){
+  return new Promise(function(resolve,reject) {
+    getNextCard(quantity, (cardsArray) => {
+      var cards = cardsArray.map((card) => {
+        return card._id;
+      })
+      var purchaseTime = new Date().toUTCString()
+      var transX = {
+        user_uuid: uuid,
+        card_uuid: cards,
+        time: purchaseTime,
+        btc_address: btcAddress,
+        fulfilled: false,
+        pending: true
+      }
+      var newTransX = new Transactions(transX);
+      newTransX.save((err, post) => {
+        if(err){
+          console.log("Error in creating transaction")
+        }
+        callback(post);
+        console.log('tx created');
+      })
+    })
+  })
 }
+
+var checkUnused = function(address){
+  return new Promise(function(resolve,reject) {
+    request(`https://blockchain.info/unspent?active=${address}`, function (error, response, body) {
+        if (!error && response.statusCode == 500 && body.includes('No free outputs to spend')) {
+          resolve(address);
+        } else {
+          reject('Possible Error: ' + error);
+        }
+    });
+  });
+};
 
 var getTransactionsByUser = function(uuid, callback){
   Transactions.find({
